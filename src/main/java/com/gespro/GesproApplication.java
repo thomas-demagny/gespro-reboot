4ce7e870281ff7612c37f0a158a292a28afe8f2d
Tom <demagny.t@gmail.com>
2022-10-03 14:47:23 +0200

    first commit
    feature: add configuration

---
+++ src/main/java/com/gespro/GesproApplication.java
@@ -0,0 +1,13 @@
+package com.gespro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gespro/GesproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gespro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
